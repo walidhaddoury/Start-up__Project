527f90a59f0dd86f7c3575bfe2faadb2315f5451
walid <walidhaddoury@outlook.fr>
2021-03-04 13:58:34 +0100

    refonte visuel

--- home/accueil.js
+++ home/accueil.js
@@ -1,13 +1,5 @@
-var slideIndex = 1;
+var slideIndex = 0;
 showSlides();
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-  
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
 
 function showSlides() {
   var i;
@@ -23,7 +15,5 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
-
-
